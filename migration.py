e2b150f8719f705c2631bc9e17767e94ace2db52
Wesley Viana <viana.wesley@gmail.com>
2019-12-24 11:49:48 -0300

    Add headers shebang, encoding, and licence

--- migration.py
+++ migration.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*-
+# This file is part of Pyway.
+#
+# Pyway is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pyway is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pyway.  If not, see <https://www.gnu.org/licenses/>.
+
+
 class Migration:
     '''
     This class represents a migration for the database.
