b6413cbe76d6a2ba72a33f738bf7117504c23a48
Wesley Viana <viana.wesley@gmail.com>
2019-12-24 11:16:52 -0300

    Basic migration class implementation

---
+++ migration.py
@@ -0,0 +1,69 @@
+import unittest
+
+
+class Migration:
+    '''
+    This class represents a migration for the database.
+    A migration is built using a major version, minor version,
+    and a name, may be built just as follow.
+
+    >>> ddl_migration  = Migration(name='database_creation', major=0, minor=0)
+
+    A migration normaly will be generate from two main places, at least what I
+    may think for now, from a file or from database.
+
+    Mostly this class will not be instanciated by contructor, but by its
+    alternative constructors, for example, from file:
+
+    >>> Migration.fromfile('V00_01__DDL.sql')
+    Migration(major=0, minor=1, name='DDL')
+
+    More about fromfile method may be found on its own documentation.
+
+    '''
+
+    def __init__(self, major, minor, name):
+        self.major = int(major)
+        self.minor = int(minor)
+        self.name = str(name)
+
+
+    @classmethod
+    def fromfile(cls, file):
+        '''
+        File may be an actual File instance or a string with a file path.
+        If no path is given before the file name, it will be assumed './'
+        >>> Migration.fromfile('V01_01__CREATE_TABLE_USER.sql')
+        Migration(major=1, minor=1, name='CREATE_TABLE_USER')
+        '''
+        from pathlib import Path
+
+        file = Path(file)
+        major, minor, name = cls.parse_filename(file.name)
+        return cls(major, minor, name)
+
+    @staticmethod
+    def parse_filename(filename):
+        '''
+        This will extract major, minor and name from a filepath string
+        >>> Migration.parse_filename('V02_01__ADDING_PARSES_PATTERNS.sql')
+        (2, 1, 'ADDING_PARSES_PATTERNS')
+        '''
+        major, minor, _, *name = filename.split('_')
+        name = '_'.join(name)
+        return int(major[1:]), int(minor), name.split('.')[0]
+
+    def __repr__(self, *args, **kwargs):
+        class_name = self.__class__.__name__
+        return "{}(major={}, minor={}, name='{}')".format(
+            class_name, self.major, self.minor, self.name)
+
+
+
+def _test():
+    import doctest
+    doctest.testmod()
+
+
+if __name__ == '__main__':
+    _test()
