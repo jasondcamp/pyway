ea9943ac4e09f3afb569d7b3692823217f73815b
Jason Camp <jason@readyset.io>
2023-02-26 16:15:52 -0500

    Fixed validate method

--- pyway/validate.py
+++ pyway/validate.py
@@ -27,29 +27,29 @@ class Validate():
             for db_migration in db_migrations:
                 logger.info("Validating --> %s" % db_migration.name)
                 local_migration = local_migrations_map.get(db_migration.version)
-                self._out_of_date(local_migration, db_migration)
-                self._name_format(local_migration.name)
-                self._diff_names(local_migration, db_migration)
-                self._diff_checksum(local_migration, db_migration)
-                logger.success("%s VALID" % db_migration.name)
+
+                if not self._out_of_date(local_migration, db_migration):
+                    logger.error(OUT_OF_DATE_ERROR % db_migration.name)
+                elif not self._name_format(local_migration.name):
+                    logger.error(VALID_NAME_ERROR % (name, Utils.expected_pattern()))
+                elif not self._diff_names(local_migration, db_migration):
+                    logger.error(DIFF_NAME_ERROR % (local_migration.name, db_migration.name))
+                elif not self._diff_checksum(local_migration, db_migration):
+                    logger.error(DIFF_CHECKSUM_ERROR % (local_migration.name, local_migration.checksum, db_migration.checksum))
+                else:
+                    logger.success("%s VALID" % db_migration.name)
 
     def _out_of_date(self, local_migration, db_migration):
-        if local_migration is None:
-            logger.error(OUT_OF_DATE_ERROR % db_migration.name)
+        return bool(local_migration is not None)
 
     def _diff_names(self, local_migration, db_migration):
-        if local_migration.name != db_migration.name:
-            logger.error(DIFF_NAME_ERROR % (local_migration.name, db_migration.name))
+        return bool(local_migration.name == db_migration.name)
 
     def _diff_checksum(self, local_migration, db_migration):
-        if local_migration.checksum != db_migration.checksum:
-            error = DIFF_CHECKSUM_ERROR % (local_migration.name, local_migration.checksum, db_migration.checksum)
-            logger.error(error)
+        return bool(local_migration.checksum == db_migration.checksum)
 
     def _name_format(self, name):
-        if not Utils.is_file_name_valid(name):
-            error = VALID_NAME_ERROR % (name, Utils.expected_pattern())
-            logger.error(error)
+        return bool(Utils.is_file_name_valid(name))
 
     def _get_all_local_migrations(self):
         local_files = Utils.get_local_files()
