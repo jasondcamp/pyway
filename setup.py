f8b947c261e53815327ccbdcc48827f85c06726b
Jason Camp <jason@readyset.io>
2023-02-27 11:57:42 -0500

    Added versions supported to PyPi

--- setup.py
+++ setup.py
@@ -28,7 +28,11 @@ setup(
     license='GPL',
     classifiers=[
       'Development Status :: 4 - Beta',
+      'Programming Language :: Python :: 3.7',
+      'Programming Language :: Python :: 3.8',
       'Programming Language :: Python :: 3.9',
+      'Programming Language :: Python :: 3.10',
+      'Programming Language :: Python :: 3.11',
     ],
     keywords='',
     packages=find_packages(exclude=['docs', 'tests*']),
